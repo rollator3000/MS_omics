b04a2e0078d7a43c09753cfb057867d16f95da19
Frederik Ludwigs <kuchenmann93@gmail.com>
2020-02-06 16:36:19 +0100

    improved the subsetting of the blocks!

--- code/03_create_needed_DFs.R
+++ code/03_create_needed_DFs.R
@@ -82,7 +82,8 @@ for (df in DFs_w_gender) {
     # 2-4-1 get the data to 'curr_data'
     curr_data <- eval(as.symbol(curr_block))
     
-    # 2-4-2 subset the data based on the block type!
+    # 2-5 subset the data based on the block type!
+    # 2-5-1 MIRNA
     if (curr_block %in% c("mirna")) {
       set.seed(subset_seed)
       sampled_cols <- sample(seq_len(ncol(curr_data)), 
@@ -91,75 +92,43 @@ for (df in DFs_w_gender) {
       
       assign(curr_block, curr_data[ ,sampled_cols])
     }
-  
-    # 2-5 Save the subsetted DF
-    save(mirna, mutation, rna, cnv, clin, 
-         file = paste0("./data/external/Dr_Hornung/Data/ProcessedData_subsets/seed_", 
-                       subset_seed, "/", df, "_subset.RData"))
     
+    # 2-5-2 MUTATION
+    if (curr_block %in% c("mutation")) {
+      set.seed(subset_seed)
+      sampled_cols <- sample(seq_len(ncol(curr_data)), 
+                             round(ncol(curr_data) * mutation_frac),
+                             replace = F)
+      
+      assign(curr_block, curr_data[ ,sampled_cols])
+    }
+    
+    # 2-5-3 CNV
+    if (curr_block %in% c("cnv")) {
+      set.seed(subset_seed)
+      sampled_cols <- sample(seq_len(ncol(curr_data)), 
+                             round(ncol(curr_data) * cnv_frac),
+                             replace = F)
+      
+      assign(curr_block, curr_data[ ,sampled_cols])
+    }
+    
+    # 2-5-5 RNA
+    if (curr_block %in% c("rna")) {
+      set.seed(subset_seed)
+      sampled_cols <- sample(seq_len(ncol(curr_data)), 
+                             round(ncol(curr_data) * rna_frac),
+                             replace = F)
+      
+      assign(curr_block, curr_data[ ,sampled_cols])
+    }
   }
   
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  # 2-2-1 Load Data and the names of the single blocks 
-  #       [except for 'target_var', as we only model classification problems!]
-  curr_blocks <- load(paste0(data_path, DF, ".Rda"))
-  curr_blocks <- curr_blocks[-which(curr_blocks %in% c("targetvar"))]
-  
-  # 2-2-2 Subset each of the blocks
-  #       mirna - remove 90%
-  mirna_ <- eval(as.symbol(curr_blocks[curr_blocks == "mirna"]))
-  set.seed(seed)
-  mirna_sub <- mirna_[, base::sample(ncol(mirna_), round(ncol(mirna_) * 0.1))]
-  
-  #       mutation - remove 90%  
-  mutation_ <- eval(as.symbol(curr_blocks[curr_blocks == "mutation"]))
-  set.seed(seed)
-  mutation_sub <- mutation_[, base::sample(ncol(mutation_), round(ncol(mutation_) * 0.1))]
-  
-  #       rna - remove 95%  
-  rna_ <- eval(as.symbol(curr_blocks[curr_blocks == "rna"]))
-  set.seed(seed)
-  rna_sub <- rna_[, base::sample(ncol(rna_), round(ncol(rna_) * 0.05))]
-  colnames(rna_sub) <- paste0(colnames(rna_sub), "_rna") # add '_rna' to colnames
-                                                         # else rna & cnv have 
-                                                         # same colnames
-  
-  #       cnv - remove 99.75%  
-  cnv_ <- eval(as.symbol(curr_blocks[curr_blocks == "cnv"]))
-  set.seed(seed)
-  cnv_sub <- cnv_[, base::sample(ncol(cnv_), round(ncol(cnv_) * 0.0025))]
-  
-  #       clin - no subset
-  clin_ <- eval(as.symbol(curr_blocks[curr_blocks == "clin"])) 
-  
-  # 2-2-4 Print how far the DF was reduced!
-  cols_org <- ncol(mirna_) + ncol(mutation_) + ncol(rna_) + ncol(cnv_) + ncol(clin_)
-  cols_red <- ncol(cbind(clin_, mirna_sub, mutation_sub, rna_sub, cnv_sub))
-  
-  print(paste0("DF: '", DF, "'"))
-  print(paste0("Reduced from orignally: ", cols_org, " features"))
-  print(paste0("To: ", cols_red, " features"))
-  
-  # 2-2-5 Save the subsetted DF!
-  #       'RData' --> load() will load all single blocks!
-  save(mirna_sub, mutation_sub, rna_sub, cnv_sub, clin_, 
-       file = paste0("./data/external/Dr_Hornung/original_processed_data_subsets/seed_", seed, "/", 
-                     DF, "_subset.RData"))
-  
-  # 2-2-6 Print info about sucessfully saving etc.
-  print(paste0("Subset for ", DF, " sucessfully saved to ", 
-               paste0("./data/external/Dr_Hornung/original_processed_data_subsets/seed_", seed, "/", 
-                      DF, "_subset.RData")))
+  # 2-6 Save the subsetted DF
+  save(mirna, mutation, rna, cnv, clin, 
+       file = paste0("./data/external/Dr_Hornung/subsetted_", subset_seed, "/", 
+                     df, "_subset.RData"))
+    
 }
 
 # [3] Get Test& OOB Performance on the subsetted DFs - JOINT BLOCKS          ----
