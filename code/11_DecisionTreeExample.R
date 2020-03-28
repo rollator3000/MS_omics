88922a7448321c70df8c0f62244ba1427caa1584
Frederik Ludwigs <kuchenmann93@gmail.com>
2020-03-28 10:10:19 +0100

    restructured the example for the decision tree in code/11

--- code/11_DecisionTreeExample.R
+++ code/11_DecisionTreeExample.R
@@ -5,7 +5,6 @@ library(ggplot2)
 library(rattle)
 library(grid)
 require(gridExtra)
-library(ggpubr)
 
 # ReCreate the data from table1 in the MS Thesis
 example <- data.frame("weight" = c(65.4, 83.9, 67.4, 105.2, 71.5, 73.0),
@@ -24,44 +23,44 @@ a1 <- fancyRpartPlot(fit)
 a2 <- ggplot(data = example, aes(x = weight, y = height, col = Y)) + 
   geom_point(size = 5) +
   theme_bw() + 
-  theme(text = element_text(size=25),
-        plot.title = element_text(size=25),
-        axis.title.x=element_blank(),
-        axis.text.x=element_blank(),
-        axis.ticks.x=element_blank()) +
-  annotate("text", x=88, y=180, label= "Node1", size = 7, col = "seagreen") +
-  scale_x_continuous(limits = c(63, 109), expand = c(0, 0)) +
-  ggtitle("Raw Data - all observations in Node1") +
-  xlab("") + 
-  ylab("")
+  theme(text = element_text(size = 22),
+        plot.title = element_text(size = 24),
+        legend.position = "none") +
+  annotate("text", x = 88, y = 180, label = "Node1", size = 8, col = "seagreen") +
+  scale_x_continuous(limits = c(63, 106), expand = c(0, 0)) +
+  ggtitle("Raw Data - all observations in Node1")
 
 a3 <- ggplot(data = example, aes(x = weight, y = height, col = Y)) + 
   geom_point(size = 5) +
   theme_bw() + 
-  theme(text = element_text(size=25),
-        plot.title = element_text(size=25),
-        axis.title.x=element_blank(),
-        axis.text.x=element_blank(),
-        axis.ticks.x=element_blank()) +
-  geom_vline(xintercept = 69, col = "red", lwd = 2) +
-  scale_x_continuous(limits = c(63, 109), expand = c(0, 0)) +
-  annotate("text", x=88, y=180, label= "Node2", size = 7, col = "seagreen") +
-  annotate("text", x=66, y=180, label= "Node3", size = 7, col = "seagreen") +
+  theme(text = element_text(size = 22),
+        plot.title = element_text(size = 24),
+        axis.title.y = element_blank(),
+        axis.text.y = element_blank(),
+        axis.ticks.y = element_blank(),
+        legend.position = "none") +
+  geom_vline(xintercept = 69, col = "red", lwd = 1.7) +
+  scale_x_continuous(limits = c(63, 106), expand = c(0, 0)) +
+  annotate("text", x = 88, y = 180, label = "Node2", size = 8, col = "seagreen") +
+  annotate("text", x = 66, y = 180, label = "Node3", size = 8, col = "seagreen") +
   ggtitle("Splitting Node1 into Node2 & Node3") + 
-  xlab("")
+  ylab("")
 
 a4 <- ggplot(data = example, aes(x = weight, y = height, col = Y)) + 
   geom_point(size = 5) +
   theme_bw() + 
-  theme(text = element_text(size=25),
-        plot.title = element_text(size=25)) +
-  geom_vline(xintercept = 69, col = "red", , lwd = 2)  +
-  geom_segment(aes(x = 69, xend = 109, y = 171, yend = 171), col = "red", lwd = 2) + 
-  scale_x_continuous(limits = c(63, 109), expand = c(0, 0),) +
-  annotate("text", x=88, y=180, label= "Node4", size = 7, col = "seagreen") +
-  annotate("text", x=80, y=169, label= "Node5", size = 7, col = "seagreen") +
-  annotate("text", x=66, y=180, label= "Node3", size = 7, col = "seagreen") +
-  ggtitle("Splitting Node 2 into Node4 & Node5") +
-  ylab("")
+  theme(text = element_text(size = 22),
+        plot.title = element_text(size = 24),
+        axis.title.y = element_blank(),
+        axis.text.y = element_blank(),
+        axis.ticks.y = element_blank()) +
+  geom_vline(xintercept = 69, col = "red", , lwd = 1.7)  +
+  geom_segment(aes(x = 69, xend = 107, y = 171, yend = 171), col = "red", lwd =1.7) + 
+  scale_x_continuous(limits = c(62, 107), expand = c(0, 0),) +
+  annotate("text", x = 88, y = 180, label = "Node4", size = 8, col = "seagreen") +
+  annotate("text", x = 80, y = 169, label = "Node5", size = 8, col = "seagreen") +
+  annotate("text", x = 65.5, y = 180, label = "Node3", size = 8, col = "seagreen") +
+  ggtitle("Splitting Node 2 into Node4 & Node5")
 
-ggarrange(a2, a3, a4, nrow = 3,common.legend = TRUE, legend="right")
+grid.arrange(a2, a3, a4, nrow = 1, top = textGrob("Single segmentation steps of a decision tree", 
+                                                  gp = gpar(fontsize = 27, font = 3)))
