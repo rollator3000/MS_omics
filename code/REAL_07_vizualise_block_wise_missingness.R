46aebbafe071f045f6cdde7af8595d4e099429c2
Frederik Ludwigs <kuchenmann93@gmail.com>
2020-05-21 18:00:34 +0200

    minor update

--- code/REAL_07_vizualise_block_wise_missingness.R
+++ code/REAL_07_vizualise_block_wise_missingness.R
@@ -69,5 +69,4 @@ ggplot(melted, aes(x = Var2, y = Var1, fill = value)) +
   theme(text = element_text(size = 22),
         axis.text.x = element_text(angle = 35, hjust = 1)) +
   ggtitle("Structure of the block-wise missingness for the clinical asthma data") +
-  geom_vline(xintercept = c(1.5, 2.5, 3.5, 4.5, 5.5), lty = 2)
-
+  geom_vline(xintercept = c(1.5, 2.5, 3.5, 4.5, 5.5), lty = 2, lwd = 1.1)
